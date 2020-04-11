5de42af48d8bcf3f8a3939254bd582fe9a0b3f97
Roman Leliuk <r.leliuk@runple.com>
2020-04-11 12:18:31 +0300

    add snake component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './store/reducers';
+import { SnakeComponent } from './components/snake/snake.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnakeComponent
   ],
   imports: [
     BrowserModule,
